39f4fec8dc57af3e584d392bca0790c559631f30
Asad Moosvi <moosvi.asad@gmail.com>
2020-07-23 20:54:30 +0500

    Add test2 for linked list speed test

---
+++ test2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "linkedlist.h"
+
+#define LEN 1000000
+
+int main(int argc, char **argv)
+{
+    list_t *l = init_list();
+    for (size_t i = 0; i < LEN; i++) {
+        printf("%zu: pushing my_string to list\r", i);
+        fflush(stdout);
+        list_push(l, "my_string");
+    }
+    printf("\n");
+
+    list_print(l);
+    return 0;
+}
+
